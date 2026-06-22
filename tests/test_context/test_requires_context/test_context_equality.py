07f9711b9c4a7f8b8bc9f9225b066f3d31e8ce3f
jiyuan0125 <jiyuan0125@126.com>
2026-06-23 05:42:00 +0800

    initial: dry-python_returns

---
+++ tests/test_context/test_requires_context/test_context_equality.py
@@ -0,0 +1,18 @@
+from collections.abc import Callable
+
+from returns.context import RequiresContext
+
+
+def _same_function(some_arg: int) -> Callable[[float], float]:
+    return lambda other: other / some_arg
+
+
+def test_equality() -> None:
+    """Ensures that containers can be compared."""
+    assert RequiresContext(_same_function) == RequiresContext(_same_function)
+
+
+def test_nonequality() -> None:
+    """Ensures that containers can be compared."""
+    assert RequiresContext(_same_function) != RequiresContext(str)
+    assert RequiresContext.from_value(1) != RequiresContext.from_value(1)
---
+++ tests/test_io/test_ioresult_container/test_ioresult_functions/test_impure_safe.py
@@ -0,0 +1,55 @@
+import pytest
+
+from returns.io import IOSuccess, impure_safe
+
+
+@impure_safe
+def _function(number: int) -> float:
+    return number / number
+
+
+@impure_safe(exceptions=(ZeroDivisionError,))
+def _function_two(number: int | str) -> float:
+    assert isinstance(number, int)
+    return number / number
+
+
+@impure_safe((ZeroDivisionError,))  # no name
+def _function_three(number: int | str) -> float:
+    assert isinstance(number, int)
+    return number / number
+
+
+def test_safe_iosuccess():
+    """Ensures that safe decorator works correctly for IOSuccess case."""
+    assert _function(1) == IOSuccess(1.0)
+
+
+def test_safe_iofailure():
+    """Ensures that safe decorator works correctly for IOFailure case."""
+    failed = _function(0)
+    assert isinstance(
+        failed.failure()._inner_value,  # noqa: SLF001
+        ZeroDivisionError,
+    )
+
+
+def test_safe_failure_with_expected_error():
+    """Ensures that safe decorator works correctly for Failure case."""
+    failed = _function_two(0)
+    assert isinstance(
+        failed.failure()._inner_value,  # noqa: SLF001
+        ZeroDivisionError,
+    )
+
+    failed2 = _function_three(0)
+    assert isinstance(
+        failed2.failure()._inner_value,  # noqa: SLF001
+        ZeroDivisionError,
+    )
+
+
+def test_safe_failure_with_non_expected_error():
+    """Ensures that safe decorator works correctly for Failure case."""
+    with pytest.raises(AssertionError):
+        _function_two('0')
